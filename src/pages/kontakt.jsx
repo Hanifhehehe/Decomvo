ad5c23cecfe82b626dce77cfccf0d2eecf9525b5
hanifhehehe <pramono.hanif@yahoo.com>
2022-10-17 16:55:25 +0700

    added more pages

--- src/pages/kontakt.jsx
+++ src/pages/kontakt.jsx
@@ -1,16 +1,39 @@
 import * as React from "react";
 import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
+import styled from "styled-components";
+
+const StyledForm = styled.div`
+  form {
+    display: grid;
+    grid-template-columns: auto-flow / 1fr 5fr;
+    ${'' /* grid-template-columns: 1fr 5fr; */}
+    textarea{
+      resize: none;
+    }
+    > * {
+      margin: 5px 0;
+    }
+    > button {
+      width: 50%;
+    }
+  }
+`
 
 const kontakt = () => {
   return (
     <Layout>
-      <h1>Kontakt.</h1>
-      <form action="https://formsubmit.co/info@decomvo.com" method="POST">
-        <input placeholder="name" type="text" name="name" required />
-        <input placeholder="email" type="email" name="email" required />
-        <textarea placeholder="Your Message" class="form-control" name="message" rows="6" required="" />
-        <button type="submit">Send</button>
-      </form> 
+      <StyledForm>
+        <h1>Kontakt.</h1>
+        <form action="https://formsubmit.co/info@decomvo.com" method="POST">
+          <label>Name</label>
+          <input type="text" name="name" required />
+          <label>Email</label>
+          <input type="email" name="email" required />
+          <label>Nachtricht</label>
+          <textarea class="form-control" name="message" rows="6" required="" />
+          <button type="submit">Send</button>
+        </form> 
+      </StyledForm>
     </Layout>
   )
 }
